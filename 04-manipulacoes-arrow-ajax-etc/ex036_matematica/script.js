be8015a742306cdf2e6663c28bea2522896868ca
Luan Cordeiro <luam.lc19@gmail.com>
2022-10-25 20:13:55 -0300

    Adicionando anotações de matemática e intervalos

---
+++ 04-manipulacoes-arrow-ajax-etc/ex036_matematica/script.js
@@ -0,0 +1,29 @@
+// retorna o valor de PI
+console.log(Math.PI);
+
+// arredonda pro número inteiro mais perto
+console.log(Math.round(3.47));
+
+// arredonda pra baixo
+console.log(Math.floor(3.47));
+
+// arredonda pra cima
+console.log(Math.ceil(3.47));
+
+// retorna o valo absoluto (positivo) de um número
+console.log(Math.abs(-9.6245));
+
+// retorna o menor número indicado
+console.log(Math.min(7, 20, 1, 245, 200));
+
+// retorna o maior número indicado
+console.log(Math.max(7, 20, 1, 245, 200));
+
+// retorna números aleatórios entre 0 e 1
+console.log(Math.random());
+
+// exemplo do uso do random
+let n1 = Math.floor(Math.random() * 100);
+
+
+console.log(n1);
---
+++ 04-manipulacoes-arrow-ajax-etc/ex037.0_intervalos/script.js
@@ -0,0 +1,20 @@
+function showTime() {
+    let d = new Date();
+
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let txt = h +  ':' +  m + ':' + s;
+    
+    document.querySelector('.demo').innerHTML = txt;
+}
+
+let timer;
+
+function start() {
+    timer = setInterval(showTime(), 1000);
+}
+
+function stop(){
+    clearInterval(timer);
+}
