38ae27b32cbb166d7b12ed96d41dbd2bb4f8a255
Luan Cordeiro <luam.lc19@gmail.com>
2022-11-04 20:35:10 -0300

    Adicionando a pasta de projetos

---
+++ projects/hash/script.js
@@ -0,0 +1,118 @@
+// Initial Data
+let square = {
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: ''
+};
+let turn = '';
+let warning = '';
+let playing = false;
+
+reset();
+
+// Events
+document.querySelector('.reset').addEventListener('click', reset);
+document.querySelectorAll(`.item`).forEach(item => {
+    item.addEventListener('click', itemClick);
+})
+
+// Functions
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item');
+    if(playing && square[item] === '') {
+        square[item] = turn;
+        renderSquare();
+        toggleTurn();
+    }
+}
+
+function reset() {
+    warning = '';
+
+    let random = Math.floor(Math.random() * 2);
+    turn = (random === 0) ? 'x' : 'o';
+    // ou use a forma longa
+    /*
+    if(random === 0) {
+        turn = 'x';
+    } else {
+        turn = 'o';
+    }
+    */
+
+    for(let i in square) {
+        square[i] = '';
+    }
+
+    playing = true;
+
+    renderSquare();
+    renderInfo();
+}
+
+function renderSquare() {
+    for(let i in square) {
+        let item = document.querySelector(`div[data-item="${i}"]`);
+        item.innerHTML = square[i];
+    }
+
+    checkGame();
+}
+
+function renderInfo() {
+    document.querySelector('.vez').innerHTML = turn;
+    document.querySelector('.resultado').innerHTML = warning;
+}
+
+function toggleTurn() {
+    turn = (turn === 'x') ? 'o' : 'x';
+    renderInfo();
+}
+
+function checkGame() {
+    if(checkWarningFor('x')) {
+        warning = 'O "x" venceu';
+        playing = false;
+    } else if(checkWarningFor('o')) {
+        warning = 'O "o" venceu';
+        playing = false;
+    } else if(isfull()) {
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+function checkWarningFor(turn) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for(let w in pos) {
+        let pArray = pos[w].split(',');
+        let hasWon = pArray.every(option => (square[option] === turn));
+        if(hasWon) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isfull() {
+    for(let i in square) {
+        if(square[i] === '') {
+            return false;
+        }
+    }
+    
+    return true;
+}
