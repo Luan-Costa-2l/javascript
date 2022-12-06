f531d2d1b87155334b961c8a48e26c5f68f7ff55
Luan Cordeiro <luam.lc19@gmail.com>
2022-12-06 20:50:15 -0300

    Adicionando anotações/códigos sobre actions na POO

--- 06-orientacao-à-objetos/ex001_orientacao-objetos/script.js
+++ 06-orientacao-à-objetos/ex001_orientacao-objetos/script.js
@@ -1,9 +1,21 @@
 class Person {
 
     age = 0;
+    steps = 0;
 
-    constructor(name, age) {
+    constructor(name) {
         this.name = name;
+    }
+
+    // actions
+    takeAStep() {
+        this.steps++;
+    }
+
+    setAge(newAge) {
+        if(typeof newAge == 'number') {
+            this.age = newAge;
+        }
     }
 }
 
@@ -16,3 +28,18 @@ p1.age = 20;
 console.log(`P1 = ${p1.name} tem ${p1.age} anos`);
 console.log(`P2 = ${p2.name} tem ${p2.age} anos`);
 console.log(`P3 = ${p3.name} tem ${p3.age} anos`);
+
+// p1.takeAStep();
+// p1.takeAStep();
+// console.log(`Passos de ${p1.name}: ${p1.steps}`)
+// console.log(`Passos de ${p2.name}: ${p2.steps}`)
+
+
+
+p1.setAge(19);
+
+console.log(`${p1.name} tem ${p1.age}`)
+
+export const ppp = () => {
+    alert('oi');
+}
