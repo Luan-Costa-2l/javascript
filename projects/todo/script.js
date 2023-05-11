78473b1120a9d549cf3066728e92ac979fff5dde
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-11 20:24:46 -0300

    feat: add task functionality
    create a function to set the task content
    
    and other to set the task on screen

--- projects/todo/script.js
+++ projects/todo/script.js
@@ -3,3 +3,22 @@ const cloneTaskModel = () => {
     const taskModelClone = taskModel.cloneNode(true);
     return taskModelClone;
 }
+
+const setTaskContent = (task, value) => {
+    const textTask = task.querySelector('.todo__listText');
+    textTask.textContent = value;
+}
+
+const setTaskOnScreen = () => {
+    const input = document.querySelector('.todo__searchInput');
+    const value = input.value.trim();
+    if (value !== '') {
+        const task = cloneTaskModel();
+        setTaskContent(task, value);
+        document.querySelector('.todo__list').appendChild(task);
+        input.value = '';
+        addDoneTaskListener();
+    }
+}
+
+document.querySelector('.todo__searchIcon').addEventListener('click', setTaskOnScreen);
